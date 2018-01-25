8528bf6b2897b02be2c30d9e3ea4a7bbc344b3bd
Jarren Long <jarrenlong@gmail.com>
2018-01-25 13:13:45 -0800

    Initial commit

---
+++ SplashActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using System.Threading.Tasks;
+
+namespace LongTech.Techie
+{
+  [Activity(Theme = "@style/TechieTheme.Splash", Icon = "@mipmap/icon", MainLauncher = true, NoHistory = true)]
+  public class SplashActivity : Activity
+  {
+    public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+    {
+      base.OnCreate(savedInstanceState, persistentState);
+
+      Window.AddFlags(WindowManagerFlags.DismissKeyguard);
+    }
+
+    protected override void OnResume()
+    {
+      base.OnResume();
+      Task startupWork = new Task(() => { LoadApp(); });
+      startupWork.Start();
+    }
+
+    public override void OnBackPressed() { }
+
+    public override void OnWindowFocusChanged(bool hasFocus)
+    {
+      base.OnWindowFocusChanged(hasFocus);
+
+      if (!hasFocus)
+      {
+        var closeDialog = new Intent(Intent.ActionCloseSystemDialogs);
+        SendBroadcast(closeDialog);
+      }
+    }
+
+    async void LoadApp()
+    {
+      // Do initialization work here ...
+      System.Threading.Thread.Sleep(3000);
+
+      StartActivity(new Intent(Application.Context, typeof(TechieActivity)));
+    }
+  }
+}
---
+++ TechieActivity.cs
@@ -0,0 +1,82 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace LongTech.Techie
+{
+  [Activity(Theme = "@style/TechieTheme", Label = "Techie", Icon = "@mipmap/icon")]
+  public class TechieActivity : Activity
+  {
+    static readonly string Tag = "ActionBarTabsSupport";
+
+    Fragment[] _fragments;
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+      base.OnCreate(savedInstanceState);
+
+      ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+      SetContentView(Resource.Layout.TechieActivity);
+
+      _fragments = new Fragment[]
+      {
+        new WhatsOnFragment(),
+        new SpeakersFragment(),
+        new SessionsFragment()
+      };
+
+      AddTabToActionBar(Resource.String.whatson_tab_label, Resource.Drawable.ic_action_whats_on);
+      AddTabToActionBar(Resource.String.speakers_tab_label, Resource.Drawable.ic_action_speakers);
+      AddTabToActionBar(Resource.String.sessions_tab_label, Resource.Drawable.ic_action_sessions);
+    }
+
+    private void AddTabToActionBar(int labelResourceId, int iconResourceId)
+    {
+      ActionBar.Tab tab = ActionBar.NewTab()
+        .SetText(labelResourceId)
+        .SetIcon(iconResourceId);
+      tab.TabSelected += TabOnTabSelected;
+      ActionBar.AddTab(tab);
+    }
+
+    private void TabOnTabSelected(object sender, ActionBar.TabEventArgs tabEventArgs)
+    {
+      tabEventArgs.FragmentTransaction.Replace(Resource.Id.frameLayoutTechie, _fragments[((ActionBar.Tab)sender).Position]);
+    }
+  }
+
+  public class SpeakersFragment : Fragment
+  {
+    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+      View view = inflater.Inflate(Resource.Layout.simple_fragment, null);
+      view.FindViewById<TextView>(Resource.Id.textView1).SetText(Resource.String.speakers_tab_label);
+      view.FindViewById<ImageView>(Resource.Id.imageView1).SetImageResource(Resource.Drawable.ic_action_speakers);
+      return view;
+    }
+  }
+
+  public class SessionsFragment : Fragment
+  {
+    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+      View view = inflater.Inflate(Resource.Layout.simple_fragment, null);
+      view.FindViewById<TextView>(Resource.Id.textView1).SetText(Resource.String.sessions_tab_label);
+      view.FindViewById<ImageView>(Resource.Id.imageView1).SetImageResource(Resource.Drawable.ic_action_sessions);
+      return view;
+    }
+  }
+
+  public class WhatsOnFragment : Fragment
+  {
+    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+      View view = inflater.Inflate(Resource.Layout.simple_fragment, null);
+      view.FindViewById<TextView>(Resource.Id.textView1).SetText(Resource.String.whatson_tab_label);
+      view.FindViewById<ImageView>(Resource.Id.imageView1).SetImageResource(Resource.Drawable.ic_action_whats_on);
+      return view;
+    }
+  }
+}
